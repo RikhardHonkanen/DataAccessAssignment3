5b1c4ffe809b72297deb0a6bfa9d0437ba44f04c
RikhardHonkanen <rikhard.h@musician.org>
2021-11-25 09:58:49 +0100

    Geolocation DLL and NuGet package added

--- DataAccessAssignment3/MainWindow.xaml.cs
+++ DataAccessAssignment3/MainWindow.xaml.cs
@@ -16,8 +16,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-//using GeographyTools;
-//using Windows.Devices.Geolocation;
+using GeographyTools;
+using Windows.Devices.Geolocation;
 
 namespace DataAccessAssignment3
 {
